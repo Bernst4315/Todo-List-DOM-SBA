b4b6a9398d06c9e064b42b2b7d71cbdb5ea245eb
bernst4315 <bernst4315@gmail.com>
2024-12-18 16:43:07 -0700

    Button is able to add items to list

--- todo.js
+++ todo.js
@@ -3,5 +3,9 @@ const addBtn = document.getElementById("add-btn");
 
 addBtn.addEventListener("click", () => {
     console.log("click recieved")
+    let listItem = document.createElement("li");
+    listItem.textContent = "Hello"
+    orderList.appendChild(listItem); 
 })
+
 console.log("Hello")
