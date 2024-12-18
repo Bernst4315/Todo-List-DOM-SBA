b7e1c830f97825e6096ec31ecd0dc34035d5be90
bernst4315 <bernst4315@gmail.com>
2024-12-18 16:39:03 -0700

    added event listener to button responds to clicks

--- todo.js
+++ todo.js
@@ -1,1 +1,7 @@
+const orderList = document.querySelector("ol"); 
+const addBtn = document.getElementById("add-btn");
+
+addBtn.addEventListener("click", () => {
+    console.log("click recieved")
+})
 console.log("Hello")
