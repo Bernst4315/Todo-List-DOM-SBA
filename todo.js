93f4a3af2cd3285e8a613a58c533e8a0ce0049e4
bernst4315 <bernst4315@gmail.com>
2024-12-19 15:51:18 -0700

    add feature to move items from todo list to done list

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
 const orderList = document.querySelector("ol"); 
 const addBtn = document.getElementById("add-btn");
+const finBtn = document.getElementById("finished-btn");
+const doneList = document.getElementById("done");
 
 addBtn.addEventListener("click", () => {
     let input = prompt("Enter ToDo Item");
@@ -10,13 +12,19 @@ addBtn.addEventListener("click", () => {
     orderList.appendChild(listItem);
     }
     //console.log("click recieved"); 
+    //add counter
 })
 
 orderList.addEventListener("click", (e) => {
     
     e.target.classList.add("done");
-    //liEl.style.color = "red";
-    console.log("click")
+    doneList.appendChild(e.target);
+    //done counter
+    //console.log("click")
+})
+
+finBtn.addEventListener("click", () => {
+    console.log("Submit")
 })
 
 console.log("Hello")
