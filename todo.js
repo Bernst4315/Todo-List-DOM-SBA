f418dc2929faef684f5136c478ea4cd718bd19a4
bernst4315 <bernst4315@gmail.com>
2024-12-19 15:15:30 -0700

    added line through text when element clicked

--- todo.js
+++ todo.js
@@ -1,14 +1,21 @@
 const orderList = document.querySelector("ol"); 
 const addBtn = document.getElementById("add-btn");
-
+let listItem = document.createElement("li");
+let liEl = document.querySelector("li");
 
 addBtn.addEventListener("click", () => {
     let input = prompt("Enter ToDo Item");
     console.log(input);
-    let listItem = document.createElement("li");
     listItem.textContent = input;
     orderList.appendChild(listItem);
-    console.log("click recieved"); 
+    //console.log("click recieved"); 
+})
+
+orderList.addEventListener("click", (e) => {
+    
+    e.target.classList.add("done");
+    //liEl.style.color = "red";
+    console.log("click")
 })
 
 console.log("Hello")
